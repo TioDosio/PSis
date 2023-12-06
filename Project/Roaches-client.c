b581ace46a714d92ef9116e03c44984aba7ec921
GuilhermeFerreiraIST <guilherme.c.ferreira@tecnico.ulisboa.pt>
2023-12-06 17:55:36 +0000

    Roach Client can now control multiple roaches

--- Project/Roaches-client.c
+++ Project/Roaches-client.c
@@ -10,11 +10,51 @@
 #include <zmq.h>
 #include <time.h>
 
+generic_msg m;
+response_msg r;
+
+void *context;
+void *requester;
+char server_address[256];
+
+void spawn_roaches(int n, int *roach_codes){
+
+    for (int i = 0; i < n; i++)
+    {
+        int points_roach = (rand() % 4) + 1;  // 1 to 4
+        
+        // Set and send message
+        m.msg_type = 0;
+        m.entity_type = ROACH;
+        m.ch = points_roach + '0';
+        zmq_send(requester, &m, sizeof(m), 0);
+
+        //Wait for response
+        zmq_recv(requester, &r, sizeof(r), 0);
+        printf("Received %d, secrect: %d\n", r.success, r.secrect_code);
+        if (r.success == 0)
+        {
+            printf("Server Full, try again later\n");
+            break;
+        }
+        roach_codes[i] = r.secrect_code;
+        }
+}
+
+
 int main(int argc, char *argv[])
 {
-    char server_address[256];
+    //Initializations
     char *server_ip = "127.0.0.1";
     char *server_port = "6666";
+    srand(time(NULL));
+    int roach_codes[MAX_ROACH_PER_CLIENT];
+
+    int sleep_delay;
+    direction_t direction;
+    int n = 0;
+    int n_roaches = rand() % MAX_ROACH_PER_CLIENT + 1; // 1 to MAX_ROACH_PER_CLIENT
+
     // para colocar o ip e a porta como argumentos
     if (argc != 3)
     {
@@ -28,56 +68,49 @@ int main(int argc, char *argv[])
 
     // creating request socket
     printf("Connecting to server…\n");
-    void *context = zmq_ctx_new();
-    void *requester = zmq_socket(context, ZMQ_REQ);
+    context = zmq_ctx_new();
+    requester = zmq_socket(context, ZMQ_REQ);
     snprintf(server_address, sizeof(server_address), "tcp://%s:%s", server_ip, server_port);
     zmq_connect(requester, server_address);
-    response_msg r;
-    srand(time(NULL));
-    int points_roach = (rand() % 4) + 1;
 
-    generic_msg m;
-    m.msg_type = 0;
-    m.entity_type = ROACH;
-    m.ch = points_roach + '0';
-    zmq_send(requester, &m, sizeof(m), 0);
-    zmq_recv(requester, &r, sizeof(r), 0);
-    printf("Received %d, secrect: %d\n", r.success, r.secrect_code);
-    if (r.success == 0)
-    {
-        printf("Server Full, try again later\n");
-        exit(0);
-    }
-    m.secrect_code = r.secrect_code;
-    int sleep_delay;
-    direction_t direction;
-    int n = 0;
+    
+    // Send n_roaches connection messages
+    spawn_roaches(n_roaches, roach_codes);
+
     while (1)
     {
-        n++;
-        sleep_delay = random() % 700000;
-        usleep(sleep_delay);
-        direction = random() % 4;
-        switch (direction)
-        {
-        case LEFT:
-            printf("%d Going Left   \n", n);
-            break;
-        case RIGHT:
-            printf("%d Going Right   \n", n);
-            break;
-        case DOWN:
-            printf("%d Going Down   \n", n);
-            break;
-        case UP:
-            printf("%d Going Up    \n", n);
-            break;
-        }
-
-        m.direction = direction;
+        m.entity_type = ROACH;
         m.msg_type = 1;
-        zmq_send(requester, &m, sizeof(m), 0);
-        zmq_recv(requester, &r, sizeof(r), 0);
+        for(int i = 0; i < n_roaches; i++)
+        {
+            sleep_delay = random() % 700000;
+            usleep(sleep_delay);
+            direction = random() % 4;
+
+            // Generate random direction
+            direction = random() % 4;
+            switch (direction)
+            {
+            case LEFT:
+                printf("%d Going Left   \n", n);
+                break;
+            case RIGHT:
+                printf("%d Going Right   \n", n);
+                break;
+            case DOWN:
+                printf("%d Going Down   \n", n);
+                break;
+            case UP:
+                printf("%d Going Up    \n", n);
+                break;
+            }
+
+            // Set and send message
+            m.direction = direction;
+            m.secrect_code = roach_codes[i];
+            zmq_send(requester, &m, sizeof(m), 0);
+            zmq_recv(requester, &r, sizeof(r), 0);
+        }
     }
     zmq_close(requester);
     zmq_ctx_destroy(context);
