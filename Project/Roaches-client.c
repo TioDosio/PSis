01ad25a6445dca6796fdd169953eba5824f7526c
andre <andre.teodosio@tecnico.ulisboa.pt>
2023-12-06 19:10:20 +0000

    Refactor spawn_roaches function and add command-line argument handling

--- Project/Roaches-client.c
+++ Project/Roaches-client.c
@@ -17,19 +17,20 @@ void *context;
 void *requester;
 char server_address[256];
 
-void spawn_roaches(int n, int *roach_codes){
+void spawn_roaches(int n, int *roach_codes)
+{
 
     for (int i = 0; i < n; i++)
     {
-        int points_roach = (rand() % 4) + 1;  // 1 to 4
-        
+        int points_roach = (rand() % 4) + 1; // 1 to 4
+
         // Set and send message
         m.msg_type = 0;
         m.entity_type = ROACH;
         m.ch = points_roach + '0';
         zmq_send(requester, &m, sizeof(m), 0);
 
-        //Wait for response
+        // Wait for response
         zmq_recv(requester, &r, sizeof(r), 0);
         printf("Received %d, secrect: %d\n", r.success, r.secrect_code);
         if (r.success == 0)
@@ -38,34 +39,44 @@ void spawn_roaches(int n, int *roach_codes){
             break;
         }
         roach_codes[i] = r.secrect_code;
-        }
+    }
 }
-
 
 int main(int argc, char *argv[])
 {
-    //Initializations
+    srand(time(NULL));
     char *server_ip = "127.0.0.1";
     char *server_port = "6666";
-    srand(time(NULL));
+    int n_roaches;
+
+    // Check if the user provided the correct number of command-line arguments
+    if (argc < 3 || argc > 4)
+    {
+        printf("Usage: %s <server_ip> <server_port> [n_roaches]\n", argv[0]);
+        return 1;
+    }
+    server_ip = argv[1];
+    server_port = argv[2];
+
+    // Set default value for n_roaches if not provided
+    if (argc == 4)
+    {
+        if (sscanf(argv[3], "%d", &n_roaches) != 1)
+        {
+            printf("Error: n_roaches must be an integer.\n");
+            return 1;
+        }
+    }
+    else
+    {
+        n_roaches = rand() % MAX_ROACH_PER_CLIENT + 1;
+    }
+
     int roach_codes[MAX_ROACH_PER_CLIENT];
 
     int sleep_delay;
     direction_t direction;
     int n = 0;
-    int n_roaches = rand() % MAX_ROACH_PER_CLIENT + 1; // 1 to MAX_ROACH_PER_CLIENT
-
-    // para colocar o ip e a porta como argumentos
-    if (argc != 3)
-    {
-        printf("You insert %d arguments, you need 3\n", argc);
-    }
-    else
-    {
-        server_ip = argv[1];
-        server_port = argv[2];
-    }
-
     // creating request socket
     printf("Connecting to server…\n");
     context = zmq_ctx_new();
@@ -73,7 +84,6 @@ int main(int argc, char *argv[])
     snprintf(server_address, sizeof(server_address), "tcp://%s:%s", server_ip, server_port);
     zmq_connect(requester, server_address);
 
-    
     // Send n_roaches connection messages
     spawn_roaches(n_roaches, roach_codes);
 
@@ -81,9 +91,9 @@ int main(int argc, char *argv[])
     {
         m.entity_type = ROACH;
         m.msg_type = 1;
-        for(int i = 0; i < n_roaches; i++)
+        for (int i = 0; i < n_roaches; i++)
         {
-            sleep_delay = random() % 700000;
+            sleep_delay = random() % 600000;
             usleep(sleep_delay);
             direction = random() % 4;
 
