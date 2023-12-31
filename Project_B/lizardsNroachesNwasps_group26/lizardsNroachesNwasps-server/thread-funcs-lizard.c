96e287c13f701dcded2d338229aaa1fa85a20076
andre <andre.teodosio@tecnico.ulisboa.pt>
2023-12-31 19:22:39 -0300

    display update

--- Project_B/lizardsNroachesNwasps_group26/lizardsNroachesNwasps-server/thread-funcs-lizard.c
+++ Project_B/lizardsNroachesNwasps_group26/lizardsNroachesNwasps-server/thread-funcs-lizard.c
@@ -3,30 +3,58 @@
 #include <pthread.h>
 #include <zmq.h>
 
+void disp_update(thread_args *shared, game_win, lines_win)
+{
+    int i = 0;
+
+    for (i = 0; i < shared->n_lizards; i++)
+    {
+        disp_clear_entity(my_win, shared->lizard_array[i]); // clear all lizards heads and bodys
+    }
+    for (i = 0; i < shared->n_lizards; i++)
+    {
+        draw_body(my_win, shared->lizard_array[i]); // draw all bodys
+    }
+    // Draw head of lizards later so it always stays on top
+    for (i = 0; i < shared->n_lizards; i++)
+    {
+        if (shared->lizard_array[i].secret_code == -1) // if lizard is disconnected???
+        {
+            continue;
+        }
+        disp_draw_entity(my_win, shared->lizard_array[i]); // draw all heads
+    }
+    // Update display
+    wrefresh(my_win);
+}
+
 void *lizard_thread(void *lizard_args)
 {
     // Load shared variables
     thread_args *shared = (thread_args *)lizard_args;
-    
-    // Initialize ZMQ 
+
+    // Initialize ZMQ
     // Socket to reply to clients
-    void *responder = zmq_socket(context, ZMQ_REP); // Create socket por REQ-REP
+    void *responder = zmq_socket(context, ZMQ_REP);    // Create socket por REQ-REP
     int rc = zmq_connect(responder, BACK_END_ADDRESS); // Bind to address
     assert(rc == 0);
 
     // Socket to send display updates
-    // void *publisher = zmq_socket(context, ZMQ_PUB); // Create socket for PUB-SUB
-    // rc = zmq_bind(publisher, ADDRESS_PUB); // Bind to address
-    // assert(rc == 0);
+    void *publisher = zmq_socket(context, ZMQ_PUB); // Create socket for PUB-SUB
+    rc = zmq_bind(publisher, ADDRESS_PUB);          // Bind to address
+    assert(rc == 0);
 
     // Initialize variables
     client_msg m;
     int code;
+    display_update update;
     int success;
 
     // loop for handling messages
-    while(1)
+    while (1)
     {
+        update.disconnect = 0;
+        update.id_l_bumped = -1;
         // Set success to 0
         success = 0;
 
@@ -35,7 +63,7 @@ void *lizard_thread(void *lizard_args)
 
         // Check if message is NOT from a lizard
         if (m.entity_type != LIZARD)
-        {   
+        {
             // Send error message
             generate_r(responder, 0, -1, 0);
             continue;
@@ -43,72 +71,69 @@ void *lizard_thread(void *lizard_args)
 
         switch (m.msg_type)
         {
-            case CONNECT:
-                // Generate Secrete code
-                code = generate_code();
+        case CONNECT:
+            // Generate Secrete code
+            code = generate_code();
 
-                //CRITICAL SECTION
-                pthread_mutex_lock(&mutex_lizard);
+            // CRITICAL SECTION
+            pthread_mutex_lock(&mutex_lizard);
 
-                // Check if there is room for more lizards
-                if (shared->n_lizards < MAX_LIZARDS)
+            // Check if there is room for more lizards
+            if (shared->n_lizards < MAX_LIZARDS)
+            {
+                // Add lizard to array
+                shared->lizard_array[shared->n_lizards].secret_code = code;
+                shared->lizard_array[shared->n_lizards].points = 0;
+                shared->n_lizards++;
+
+                // success
+                success = 1;
+            }
+
+            // END CRITICAL SECTION
+            pthread_mutex_unlock(&mutex_lizard);
+
+            // Send response [HERE] MUST SEND DISPLAY_CONNECT?
+            generate_r(responder, success, code, 0);
+
+            break;
+
+        case DISCONNECT:
+            // CRITICAL SECTION
+            pthread_mutex_lock(&mutex_lizard);
+
+            // Check if lizard exists
+            for (int i = 0; i < shared->n_lizards; i++)
+            {
+                if (shared->lizard_array[i].secret_code == m.secret_code)
                 {
-                    // Add lizard to array 
-                    shared->lizard_array[shared->n_lizards].secret_code = code;
-                    shared->lizard_array[shared->n_lizards].points = 0;
-                    shared->n_lizards++;
+                    // Remove lizard from array
+                    shared->lizard_array[i].secret_code = -1;
+                    shared->n_lizards--;
 
                     // success
                     success = 1;
-
+                    update.disconnect = 1;
+                    break;
                 }
+            }
 
-                //END CRITICAL SECTION
-                pthread_mutex_unlock(&mutex_lizard);
-                
-                // Send response [HERE] MUST SEND DISPLAY_CONNECT?
-                generate_r(responder, success, code, 0);
-                 
-                break;
+            // END CRITICAL SECTION
+            pthread_mutex_unlock(&mutex_lizard);
 
-            case DISCONNECT:
-                //CRITICAL SECTION
-                pthread_mutex_lock(&mutex_lizard);
+            // Send response
+            generate_r(responder, success, m.secret_code, 0);
 
-                // Check if lizard exists
-                for (int i = 0; i < shared->n_lizards; i++)
-                {
-                    if (shared->lizard_array[i].secret_code == m.secret_code)
-                    {
-                        // Remove lizard from array
-                        shared->lizard_array[i].secret_code = -1;
-                        shared->n_lizards--;
+            break;
 
-                        // success
-                        success = 1;
-                        break;
-                    }
-                }
-
-                //END CRITICAL SECTION
-                pthread_mutex_unlock(&mutex_lizard);
-
-                // Send response
-                generate_r(responder, success, m.secret_code, 0);
-
-                break;
-
-            case MOVE:
-                move_lizard(m.secret_code, m.content, shared);
-
-                break;
-
+        case MOVE:
+            move_lizard(m.secret_code, m.content, shared);
+            break;
         }
-
+        update.entity = shared->lizard_array[m.secret_code];
         // Update display
-        update_display(shared);
+        disp_update(shared, game_win, lines_win); // need to pass this arguments to this function from maint ot write to display
         // Send display update to lizard-clients
-        // zmq_send(publisher, &update, sizeof(update), 0);
-
+        zmq_send(publisher, &update, sizeof(update), 0);
     }
 }
--- Project_B/lizardsNroachesNwasps_group26/lizardsNroachesNwasps-server/thread-funcs.h
+++ Project_B/lizardsNroachesNwasps_group26/lizardsNroachesNwasps-server/thread-funcs.h
@@ -7,27 +7,29 @@ pthread_mutex_t mutex_npc;
 
 /**
  * @brief Thread function for handeling lizard messages and execute actions
- * 
+ *
  * @param lizard_args pointer to struct with arguments for the thread
- * 
+ *
  */
 void *lizard_thread(void *lizard_args);
 
 /**
  * @brief Thread function for handeling NPC messages and execute actions
- * 
+ *
  * @param lizard_args pointer to struct with arguments for the thread
- * 
+ *
  */
 void *npc_thread(void *lizard_args);
 
 /**
  * @brief Function to generate and send response to lizard client
- * 
+ *
  * @param responder socket to send response
  * @param success success of operation
  * @param secret_code secret code of entity
  * @param score score of entity (if applicable)
- * 
+ *
  */
 void generate_r(void *responder, int success, int secret_code, int score);
+
+void disp_update(thread_args *shared);
