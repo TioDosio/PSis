4329f43b271d77f5c5ea454224d090520ff1909e
andre <andre.teodosio@tecnico.ulisboa.pt>
2024-01-06 06:28:21 -0300

    wasps cenas

--- Project_B/lizardsNroachesNwasps_group26/Wasp-client/Wasp-client.py
+++ Project_B/lizardsNroachesNwasps_group26/Wasp-client/Wasp-client.py
@@ -1,11 +1,11 @@
 import zmq
-import struct
 import sys
+from messages_pb2 import ClientMessage, ResponseMessage
 
 class EntityType:
     LIZARD = 0
     ROACH = 1
-    DISPLAY = 2
+    WASP = 2
 
 class Direction:
     UP = 0
@@ -13,70 +13,86 @@ class Direction:
     LEFT = 2
     RIGHT = 3
 
+class MessageType:
+    CONNECT = 0
+    MOVE = 1
+    DISCONNECT = 2
+
 class Entity:
-    def __init__(self, entity_type, ch, points, pos_x, pos_y, direction, secret_code):
-        self.entity_type = entity_type
-        self.ch = ch
-        self.points = points
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.direction = direction
-        self.secret_code = secret_code
+    def __init__(self):
+        self.entity_type = None  # type of entity
+        self.ch = None  # ch of entity
+        self.points = None  # amount of points
+        self.pos_x = None  # position of entity
+        self.pos_y = None  # position of entity
+        self.direction = None  # direction of last movement
+        self.secret_code = None  # secret code to send
 
-class GenericMsg:
-    def __init__(self, entity_type, msg_type, ch, direction, secret_code):
-        self.entity_type = entity_type
-        self.msg_type = msg_type
-        self.ch = ch
-        self.direction = direction
-        self.secret_code = secret_code
+class ClientMessage:
+    def __init__(self):
+        self.entity_type = None  # type of entity
+        self.msg_type = None  # type of message
+        self.content = None  # value to send (points, char, movement direction, etc)
+        self.secret_code = None  # secret id for entity
 
-def send_generic_msg(socket, generic_msg):
-    packed_data = struct.pack('I B c B H H B h',
-        generic_msg.entity_type,
-        generic_msg.msg_type,
-        bytes([ord(generic_msg.ch)]),
-        generic_msg.direction,
-        0,  # Padding byte for alignment
-        0,  # Padding short int for alignment
-        0,  # Padding byte for alignment
-        generic_msg.secret_code
-    )
-    socket.send(packed_data)
+class ResponseMessage:
+    def __init__(self):
+        self.success = None  # 0 - fail, 1 - success, 2 - success in disconnection
+        self.score = None  # score of the player
+        self.secret_code = None  # secret id of entity
+
+def send_and_receive_message(entity_type, msg_type, content, secret_code):
+    # Create a ClientMessage
+    client_msg = ClientMessage()
+    client_msg.entity_type = entity_type
+    client_msg.msg_type = msg_type
+    client_msg.content = content
+    client_msg.secret_code = secret_code
+
+    # Serialize the message
+    msg_bytes = client_msg.SerializeToString()
+
+    # Send the message
+    socket.send(msg_bytes)
+
+    print(f"Sent {msg_type} message to the server")
+
+    # Receive the response
+    resp_bytes = socket.recv()
+
+    # Parse the response
+    response_msg = ResponseMessage()
+    response_msg.ParseFromString(resp_bytes)
+
+    print("Received response from the server:")
+    print(f"Success: {response_msg.success}")
+    print(f"Score: {response_msg.score}")
+    print(f"Secret code: {response_msg.secret_code}")
+    
 
 def main(server_ip, server_port):
-    try:
-        # Convert server_port to an integer
-        server_port = int(server_port)
-    except ValueError:
-        print("Error: Invalid server_port. Please provide a valid integer.")
-        sys.exit(1)
-
-    # Define the server address
-    server_address = f"tcp://{server_ip}:{server_port}"
-
-    # Create a ZeroMQ context and socket
-    context = zmq.Context()
-    socket = context.socket(zmq.PUSH)
-
     # Connect to the server
-    socket.connect(server_address)
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect(f"tcp://{server_ip}:{server_port}")
 
-    # Create a generic_msg instance and send it
-    generic_message = GenericMsg(
-        entity_type=EntityType.ROACH,
-        msg_type=1,  # 0 - connect, 1 - move, 2 - disconnect
-        ch='A',      # Your value to send (points of roaches or char of lizard)
-        direction=Direction.RIGHT,
-        secret_code=123
-    )
 
-    # Send the generic message
-    send_generic_msg(socket, generic_message)
 
-    # Close the socket and context
+
+
+
+
+
+
+    entity_type = 1  # Update with the desired entity type
+    msg_type = 0  # Update with the desired message type
+    content = "Some content"
+    secret_code = 123
+    send_and_receive_message(entity_type, msg_type, content, secret_code)
+
     socket.close()
-    context.term()
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -85,5 +101,5 @@ if __name__ == "__main__":
 
     server_ip = sys.argv[1]
     server_port = sys.argv[2]
-
     main(server_ip, server_port)
+    
